b0e36d6252aebfa3185f3542eb9ff4105c756a2d
thang241190 <thangnguyen24111990@gmail.com>
2021-04-20 23:15:31 +0300

    Finish ex4.3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import {
+	Container,
+	Grid,
+	Paper,
+	Box,
+	Button,
+	Typography,
+} from "@material-ui/core"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	paper: {
+		margin: theme.spacing(2),
+		padding: theme.spacing(2, 1),
+		textAlign: "center",
+		color: theme.palette.text.secondary,
+	},
+	text: {
+		border: "1px solid #333",
+		padding: "0 10px",
+	},
+	box: {
+		paddingTop: 20,
+	},
+	button: {
+		padding: 0,
+		"& .MuiButton-label": {
+			padding: 6,
+		},
+	},
+}))
+
+export default function App() {
+	const classes = useStyles()
+	const [value, setValue] = useState("")
+	const [prevString, setPrevString] = useState([])
+	const [current, setCurrent] = useState(0)
+	const addNumber = (num) => {
+		setValue(value + num)
+		setPrevString([...prevString, value])
+		setCurrent(current + 1)
+		console.log(prevString)
+	}
+	const reset = () => {
+		setValue("")
+	}
+	const undo = () => {
+		setValue(prevString[current - 1])
+
+		setCurrent(current - 1)
+	}
+	const redo = () => {
+		setValue(prevString[current + 1])
+		setCurrent(current + 1)
+	}
+
+	return (
+		<div className={classes.root}>
+			<Container maxWidth='sm'>
+				<Grid container>
+					<Grid item xs={2}></Grid>
+					<Grid item xs={8}>
+						<Paper elevation={2} className={classes.paper}>
+							<Typography>{value}</Typography>
+							<Box className={classes.box}>
+								<Button
+									variant='contained'
+									onClick={() => addNumber(1)}
+									color='primary'
+									className={classes.button}>
+									1
+								</Button>
+								<Button
+									variant='contained'
+									color='primary'
+									onClick={() => addNumber(2)}
+									className={classes.button}>
+									2
+								</Button>
+								<Button
+									variant='contained'
+									color='primary'
+									onClick={() => addNumber(3)}
+									className={classes.button}>
+									3
+								</Button>
+							</Box>
+							<Box className={classes.box}>
+								{current !== 0 ? (
+									<Button
+										variant='contained'
+										color='secondary'
+										onClick={undo}>
+										Undo
+									</Button>
+								) : (
+									<Button
+										variant='contained'
+										color='secondary'
+										disabled>
+										Undo
+									</Button>
+								)}
+								{current < prevString.length ? (
+									<Button
+										variant='contained'
+										color='primary'
+										onClick={redo}>
+										Redo
+									</Button>
+								) : (
+									<Button
+										variant='contained'
+										color='primary'
+										disabled>
+										Redo
+									</Button>
+								)}
+
+								<Button
+									variant='contained'
+									color='primary'
+									onClick={reset}>
+									Reset
+								</Button>
+							</Box>
+						</Paper>
+					</Grid>
+					<Grid item xs={2}></Grid>
+				</Grid>
+			</Container>
+		</div>
+	)
 }
-
-export default App;
